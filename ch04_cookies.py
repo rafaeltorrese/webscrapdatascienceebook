39e59bf0e2a47cbc3ca7f32de73f5dd2f77b7ffb
Rafael Torres Escobar <torresc.rafael@gmail.com>
2022-06-02 13:32:23 -0500

    cookies for authtentication

--- ch04_cookies.py
+++ ch04_cookies.py
@@ -1,10 +1,20 @@
 import requests
 
 
-url = 'http://www.webscrapingfordatascience.com/cookielogin/secret.php'
+url = 'http://www.webscrapingfordatascience.com/cookielogin/'
 
-my_cookies = {'PHPSESSID':'co6ufb3t25mfv4ohgu20v7h3mm'}
-r = requests.get(url, cookies=my_cookies)
+# First perform a POST request
+r = requests.post(url, data={'username': 'dummy', 'password':'1234'})
 
+# Get the cookie value, either from
+# r.header or r.cookies print(r.cookies)
+my_cookies = r.cookies
+
+# r.cookies is a RequestsCookieJar object which can also be accesed like a dictionary. The following als works:
+my_cookies['PHPSESSID'] = r.cookies.get('PHPSESSID')
+
+# Now perform a GET request to the secret page usign the cookies
+page = 'secret.php'
+r = requests.get(f'{url}{page}', cookies=my_cookies)
 
 print(r.text)
