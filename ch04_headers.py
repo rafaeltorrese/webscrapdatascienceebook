7d39775271f07b138205a72529c3216471ccd48c
Rafael Torres Escobar <torresc.rafael@gmail.com>
2022-06-01 20:51:27 -0500

    including headers

--- ch04_headers.py
+++ ch04_headers.py
@@ -2,6 +2,10 @@ import requests
 
 url = 'http://www.webscrapingfordatascience.com/usercheck/'
 
-r = requests.get(url)
+my_headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 ' + ' (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36'
+}
+
+r = requests.get(url, headers=my_headers)
 print(r.text)
 print(r.request.headers)
